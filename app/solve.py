45ca3ffdf131b37b6e303d337d972768e1127b78
danhhuit <nguyenthanhdanh2001230095@gmail.com>
2025-09-23 20:52:15 +0700

    add algorithm

--- app/solve.py
+++ app/solve.py
@@ -1,3 +1,4 @@
+import statistics
 import statistics as stats
 import time
 import math
@@ -363,3 +364,231 @@ class AStarAlgorithm(SudokuAlgorithm):
                 f_value=h_value+new_g_value
                 heapq.heappush(priority_queue, (f_value, new_g_value, new_board))
         return False
+#giai bang giai thuat mo phong luyen kim ***
+class SimulatedAnnealing(SudokuAlgorithm):
+    def solve(self):
+        start_time = time.time()
+        self.states_explored = 0
+        self.max_states_in_memory = 1
+
+        fixed_cells = self._create_fixed_cells()
+        list_of_blocks = self._create_list_of_blocks()
+        self._randomly_fill_blocks(fixed_cells, list_of_blocks)
+
+        result = self._simulated_annealing(fixed_cells, list_of_blocks)
+
+        self.execution_time = time.time() - start_time
+        self.is_solved = result
+        self.solution = cpy.deepcopy(self.board) if result else None
+
+        if result: self.h_value = 0
+        else: self.h_value = self._calculate_number_of_errors()
+
+        self.f_value=self.g_value+self.h_value
+        return result
+
+    #tao mtran danh dau cac o co dinh trong bang Sudoku
+    def _create_fixed_cells(self):
+        fixed_cells = [[0 for _ in range(self.grid_size)] for _ in range(self.grid_size)]
+        for i in range(self.grid_size):
+            for j in range(self.grid_size):
+                if self.board[i][j] != 0:
+                    fixed_cells[i][j] = 1
+        return fixed_cells
+
+    #tao danh sach cac khoi trong bang sudoku
+    def _create_list_of_blocks(self):
+        final_list = []
+        block_size = self.grid_size
+        for r in range(0, self.grid_size):
+            tmp_list = []
+            #b1:block 1, b2: block 2
+            b1 = [i+block_size *((r)%block_size) for i in range(0, block_size)]
+            b2 = [i+block_size *math.trunc((r)/block_size) for i in range(0, block_size)]
+            for i in b1:
+                for j in b2:
+                    tmp_list.append([i,j])
+            final_list.append(tmp_list)
+        return final_list
+
+    #dien ngau nhien cac gtri vao cac khoi
+    def _randomly_fill_blocks(self, fixed_cells, list_of_blocks):
+        #block:bl, box:bx
+        for bl in list_of_blocks:
+            for bx in bl:
+                if self.board[bx[0]][bx[1]] == 0:
+                    current_block_values = []
+                    for b in bl:
+                        current_block_values.append(self.board[b[0]][b[1]])
+                    possible_values = [i for i in range(1, self.grid_size+1) if i not in current_block_values]
+                    if possible_values:
+                        self.board[bx[0]][bx[1]] = rand.choice(possible_values)
+
+    #tinh tong so loi tren toan bo bang
+    def _calculate_number_of_errors(self):
+        error_count=0
+        for i in range(self.grid_size):
+            error_count+=self._calculate_number_row_col(i,i)
+        return error_count
+    #tinh so loi
+    def _calculate_number_row_col(self, row, col):
+        col_values=\
+            [self.board[i][col] for i in range(self.grid_size) if self.board[i][col]!=0]
+        unique_col_values = set(col_values)
+        col_er = len(col_values)-len(unique_col_values)
+
+        row_values = \
+            [self.board[row][j] for j in range(self.grid_size) if self.board[row][j] != 0]
+        unique_row_values = set(row_values)
+        row_er = len(row_values) - len(unique_row_values)
+
+        return col_er + row_er
+    #chon ngau nhien hai o k co dinh trong 1 khoi
+    def _two_random_boxes_within_block(self, fixed_cells, block):
+        non_fixed_boxes = \
+            [box for box in block if fixed_cells[box[0]][box[1]]!=1]
+        if len(non_fixed_boxes)<2: return None
+        first_box = rand.choice(non_fixed_boxes)
+        second_box = rand.choice([box for box in non_fixed_boxes if box != first_box])
+        return [first_box, second_box]
+
+    #hoan doi vi tri 2box
+    def _flip_boxes(self, boxes):
+        proposed_board = cpy.deepcopy(self.board)
+        temp = proposed_board[boxes[0][0]][boxes[0][1]]
+        proposed_board[boxes[0][0]][boxes[0][1]] = proposed_board[boxes[1][0]][boxes[1][1]]
+        proposed_board[boxes[1][0]][boxes[1][1]] = temp
+        return proposed_board
+
+    #tao trang thai de xuat (hoan doi 2 box k co dinh trong 1 block)
+    def _proposed_state(self, fixed_cells, list_of_blocks):
+        shuffled_block = rand.sample(list_of_blocks, len(list_of_blocks))
+
+        for rand_block in shuffled_block:
+            fixed_count=sum(fixed_cells[box[0]][box[1]] for box in rand_block)
+            max_fixed = 6 if self.grid_size == 9 else 12
+            if fixed_count > max_fixed: continue
+            boxes_to_flip = self._two_random_boxes_within_block(fixed_cells, rand_block)
+            if boxes_to_flip:
+                proposed_board = self._flip_boxes(boxes_to_flip)
+                return proposed_board, boxes_to_flip
+        return self.board, None
+
+    #tao trang thai moi theo chi phi va nhiet do
+    #sigma: nhiet do htai
+    def _choose_new_state(self, fixed_cells, list_of_blocks, sigma):
+        (proposed_board,
+         boxes_to_flip)= self._proposed_state(fixed_cells, list_of_blocks)
+        if boxes_to_flip is None: return self.board, 0
+        cur_board = cpy.deepcopy(self.board)
+        cur_cost = 0
+        for box in boxes_to_flip:
+            cur_cost+=self._calculate_number_of_errors(box[0], box[1])
+        self.board=proposed_board
+        new_cost = 0
+        for box in boxes_to_flip:
+            new_cost+=self._calculate_number_of_errors(box[0], box[1])
+
+        cost_diff = new_cost - cur_cost
+        rho = math.exp(-cost_diff/sigma)
+
+        if rand.uniform(0,1) < rho: return proposed_board, boxes_to_flip
+        else:
+            self.board=cur_board
+            return cur_board, 0
+
+    #tinh gtri sigma ban dau (dua tren do lech chuan cua cac chi phi)
+    def _calculate_initial_sigma(self, fixed_cells, list_of_blocks):
+        list_of_diff = []
+        cur_board = cpy.deepcopy(self.board)
+        tmp_board = cpy.deepcopy(self.board)
+
+        for i in range(10):
+            (proposed_board,
+             boxes_to_flip) = self._proposed_state(fixed_cells, list_of_blocks)
+            if boxes_to_flip:
+                self.board=proposed_board
+                list_of_diff.append(self._calculate_number_of_errors())
+        self.board = tmp_board
+
+        if len(list_of_diff)>1: return stats.pstdev(list_of_diff)
+        else: return 1.0
+
+    #tinh so lan lap dua tren so o co dinh
+    def _choose_number_of_iterations(self, fixed_cells):
+        fixed_count=sum(sum(row) for row in fixed_cells)
+        base_iterations=100
+        if self.grid_size == 16: base_iterations=200
+        return max(fixed_count, base_iterations)
+
+    #thuat toan mo phong luyen kim
+    def _simulated_annealing(self, fixed_cells, list_of_blocks):
+        solution_found = False
+        attempts = 0
+        max_attempts = 3
+
+        while not solution_found and attempts < max_attempts:
+            attempts += 1
+            self.states_explored += 1
+
+            if attempts > 1:
+                self.board = [[0 if fixed_cells[i][j] == 0 else self.board[i][j]
+                               for j in range(self.grid_size)]
+                              for i in range(self.grid_size)]
+                self._randomly_fill_blocks(fixed_cells, list_of_blocks)
+
+            decrease_factor = 0.99
+            stuck_count = 0
+
+            sigma = self._calculate_initial_sigma(fixed_cells, list_of_blocks)
+
+            score = self._calculate_number_of_errors()
+
+            iterations = self._choose_number_of_iterations(fixed_cells)
+
+            if score <= 0:
+                solution_found = True
+                break
+
+            max_loops = 500
+            current_loop = 0
+
+            while not solution_found and current_loop < max_loops:
+                current_loop += 1
+                previous_score = score
+
+                for i in range(iterations):
+                    new_board, score_diff = self._choose_new_state(fixed_cells, list_of_blocks, sigma)
+
+                    self.board = new_board
+                    score += score_diff
+
+                    self.states_explored += 1
+                    self.g_value += 1 if score_diff != 0 else 0
+
+                    if score <= 0:
+                        solution_found = True
+                        break
+
+                sigma *= decrease_factor
+
+                if score <= 0:
+                    solution_found = True
+                    break
+
+                if score >= previous_score:
+                    stuck_count += 1
+                else:
+                    stuck_count = 0
+
+                if stuck_count > 80:
+                    sigma += 2
+
+                if self._calculate_number_of_errors() == 0:
+                    solution_found = True
+                    break
+
+                if sigma < 0.001:
+                    break
+
+        return solution_found
